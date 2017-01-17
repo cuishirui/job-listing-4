f92452a7361bf6c9684de22ed742d52c5f03f670
cuishirui <344494024@qq.com>
2017-01-17 21:35:55 +0800

    装修网站

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,13 @@ Rails.application.routes.draw do
         post :hide
       end
 
-      resources :resumes 
+      resources :resumes
     end
   end
-  root 'jobs#index'
+
+  resources :welcome do
+
+  end
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
